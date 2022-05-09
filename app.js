450ff656eeebbb80ed7b47953d5860c6b3d1c320
luka1901 <zga655426@gmail.com>
2022-05-10 07:55:05 +0800

    20220510 first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function getData() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(function (res) {
+            return res.json();
+        }).then(function (data) {
+            let dataDiv = document.querySelector("#dataDiv");
+            dataDiv.innerHTML = "";
+            for (let i = 0; i < data.length; i++) {
+                let hako = data[i];
+                dataDiv.innerHTML += "<p>" + hako.userId + ": " + hako.title + "</p>";
+            }
+
+        })
+};
